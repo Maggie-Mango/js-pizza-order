a981c0f5d0706fa8d69c9f48f0f08fbf813356ad
Maggie S <Maggiesaldivia@gmail.com>
2021-02-27 00:29:11 -0800

    shows prices correctly when selecting pizza size.

--- js/scripts.js
+++ js/scripts.js
@@ -4,23 +4,24 @@ function Customer(name) {
   this.name = name;
 }
 
-function Pizza(size, toppings) {
+function Pizza(size) {
   this.size = size;
-  this.toppings = toppings;
 }
 
 Pizza.prototype.pizzaCost = function() {
 	price = 0
-	sizePrice = {"S":10,"M":13.5,"L":16,"XL":18}
-  toppings = this.toppings
+	sizePrice = {"SM":10,"M":13.5,"L":16,"XL":18}
 	for (let key in sizePrice) {
-		if ((this.size === key) && (toppings.length > 2)) {
-    toppings = (toppings.length - 2)*2
-		price += sizePrice[key] + toppings
-		return price
+		if ((this.size === key) && (pizzaToppings.length === 1)) {
+      price = sizePrice[key];
+      alert(price);
+    } else if ((this.size === key) && (pizzaToppings.length > 1)) {
+      price = sizePrice[key];
+      alert(price + ((pizzaToppings.length -1)*2))
+     }
     }
-  }
-}
+  }  
+
 
 Pizza.prototype.receipt = function() {
   return "1 " + this.size + " pizza with " + this.toppings
@@ -31,19 +32,21 @@ Pizza.prototype.receipt = function() {
 //console.log(testPizza.pizzaCost())
 //console.log(testPizza.toppings.length)
 
-
+pizzaToppings = []
 //UI logic
 $(document).ready(function() {
   $("form#new-order").submit(function(event) {
     event.preventDefault();
     const inputtedName = $("input#name").val();
     //console.log(inputtedName)
-    $("input:checkbox[name=pizzaTopping]:checked").each(function() {
-      const pizzaTopping = $(this).val();
-      //console.log(pizzaTopping)
+  $("input:checkbox[name=pizzaTopping]:checked").each(function() {
+      pizzaToppings.push($(this).val());
     });
     const pizzaSize = $(".form-select option:selected").text();
     //alert(pizzaSize)
-    const newOrder = new Customer
+    let customer = new Customer(inputtedName);
+    let newPizza = new Pizza(pizzaSize);
+    //alert(pizzaToppings.length)
+    newPizza.pizzaCost()
   });
 });
